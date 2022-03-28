7bee9db4049bdcf641398ba1d28783242a5ac292
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2022-03-28 20:46:19 +0530

    basic

---
+++ main.js
@@ -0,0 +1,71 @@
+
+const quizContainer=document.getElementById('quiz');
+const resultsContainer=document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const myQuestions=[
+    {
+        question:"Who invented JavaScript ?",
+        answers:{
+            a:"Douglas Crockford",
+            b:"Sheryl Sandberg",
+            c:"Brendan Eich"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question:"Which one of these is a JavaScript package manager?",
+        answers:{
+            a:'NodeJs',
+            b:'TypeScript',
+            c:'npm'
+        },
+        correctAnswer:"c"
+    },
+    {
+        question:"Which tool can you use to ensure code quality ? ",
+        answers:{
+            a: "Angular",
+            b: "Jquery",
+            c: "RequireJS",
+            d: "ESLint"
+        },
+        correctAnswer:"d"
+    }
+];
+
+
+function buildQuiz(){
+        const output=[];
+        myQuestions.forEach(
+            (currentQuestion,questionNumber)=>{
+                const answers=[];
+                for (letter in currentQuestion.answers){
+                    answers.push(
+                        `<label>
+                        <input type="radio" name="question${questionNumber} value="${letter}">
+                        ${letter} : 
+                        ${currentQuestion.answers[letter]}
+                        </label>`
+                    );
+                }
+                output.push(
+                    `<div class="question>${currentQuestion.question}</div>
+                    <div class="answers">${answers.join(' ')}</div>`
+                );
+            }
+
+        );
+        quizContainer.innerHTML=output.join(' ');
+}
+
+function showResults(){
+
+}
+
+buildQuiz();
+
+submitButton.addEventListener('click',showResults);
+
+
+
